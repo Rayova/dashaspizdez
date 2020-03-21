4fe7dd7cfad1320a516c2d547eb695ecebba9554
Rayova <55283119+Rayova@users.noreply.github.com>
2020-03-21 22:27:29 +0300

    b

---
+++ js/alert.js
@@ -0,0 +1,50 @@
+// circle13
+// let circleAnimation = anime({
+//   targets: circle,
+//   rotate: [0, 360],
+//   easing: 'linear',
+//   duration: 50000,
+//   loop: true
+// });
+//
+// icon_12
+// let iconAnimation = anime ({
+//   targets: icon,
+//   rotate: [0, 360],
+//   autoplay: false,
+//   duration: 2000
+// })
+// let icon01 = document.getElementById('icon')
+// icon01.onclick = iconAnimation.play
+
+// alert
+
+  let guest1 = prompt("Please enter your name", "Hot stuff");
+  if (guest1 != null) {
+    document.getElementById("guest").innerHTML = guest1
+  }
+  alert()
+
+// // кнопка блоки
+// let blocks = document.querySelector('.blocks');
+// let icon = document.querySelector('.icon');
+// blocks.style.display = 'none'
+//
+// icon.addEventListener("click",      function() {
+//
+//     if(blocks.style.display == 'none')  blocks.style.display = 'block';
+//     else blocks.style.display = 'none'
+//
+//   });
+//
+// // кнопка  прямоугольники
+// let rectangles = document.querySelector('.rectangles');
+// let tool2 = document.querySelector('.tool_2');
+// rectangles.style.display = 'none'
+//
+// tool2.addEventListener("click",      function() {
+//     console.log(2345678);
+//     if(rectangles.style.display == 'none')  rectangles.style.display = 'block';
+//     else rectangles.style.display = 'none'
+//
+//   });
