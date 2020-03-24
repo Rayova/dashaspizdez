1a36788293095ab8eb1d940bb60388a2b28d3225
Rayova <55283119+Rayova@users.noreply.github.com>
2020-03-24 23:56:40 +0300

    рррр

--- js/effects.js
+++ js/effects.js
@@ -1,24 +1,24 @@
-/* burger */
-let yellow = document.querySelector('.blink__1');
-let puller = document.querySelector('.main__section__5__img__2');
-yellow.style.display = 'none'
-
-puller.addEventListener("click",      function() {
-
-   if(yellow.style.display == 'none')  yellow.style.display = 'flex';
-   else yellow.style.display = 'none'
-
- });
-
- let pull1 = document.querySelector('.main__section__5');
- let button_3 = document.querySelector('.button__3');
- pull1.style.display = 'none'
-
- button_3.addEventListener("click",      function() {
-
-    if(pull1.style.display == 'none')  pull1.style.display = 'flex';
-    else pull1.style.display = 'none'
-
-  });
-
-/* text */
+// /* burger */
+// let yellow = document.querySelector('.blink__1');
+// let puller = document.querySelector('.main__section__5__img__2');
+// yellow.style.display = 'none'
+//
+// puller.addEventListener("click",      function() {
+//
+//    if(yellow.style.display == 'none')  yellow.style.display = 'flex';
+//    else yellow.style.display = 'none'
+//
+//  });
+//
+//  let pull1 = document.querySelector('.main__section__5');
+//  let button_3 = document.querySelector('.button__3');
+//  pull1.style.display = 'none'
+//
+//  button_3.addEventListener("click",      function() {
+//
+//     if(pull1.style.display == 'none')  pull1.style.display = 'flex';
+//     else pull1.style.display = 'none'
+//
+//   });
+//
+// /* text */
