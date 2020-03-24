09294e9cb85ee3e8dcc6e98d912c1b63244f15b8
Rayova <55283119+Rayova@users.noreply.github.com>
2020-03-24 21:19:34 +0300

    fgbf

--- js/wheel.js
+++ js/wheel.js
@@ -6,3 +6,26 @@ let wheelAnimation = anime ({
   duration: 50000,
   loop: true
 });
+let wheel1Animation = anime ({
+  targets: wheel2,
+  rotate: [0, 360],
+  easing: 'linear',
+  duration: 5000,
+  loop: true
+});
+let wheel2Animation = anime ({
+  targets: wheel3,
+  rotate: [0, 180],
+  easing: 'linear',
+  duration: 8000,
+  loop: true,
+  translateY: 150
+});
+let wheel3Animation = anime ({
+  targets: wheel4,
+  rotate: [0, -360],
+  easing: 'linear',
+  duration: 20000,
+  loop: true,
+  translateX: 230
+});
