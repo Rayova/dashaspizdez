7113d0f02e3f13f68a10c24ce9b80369d173c79a
Rayova <55283119+Rayova@users.noreply.github.com>
2020-03-26 08:49:09 +0300

    hfvjknr

--- js/wheel.js
+++ js/wheel.js
@@ -1,31 +1,31 @@
 /* колесо */
-let wheelAnimation = anime ({
-  targets: wheel1,
-  rotate: [0, 360],
-  easing: 'linear',
-  duration: 50000,
-  loop: true
-});
-let wheel1Animation = anime ({
-  targets: wheel2,
-  rotate: [0, 360],
-  easing: 'linear',
-  duration: 5000,
-  loop: true
-});
-let wheel2Animation = anime ({
-  targets: wheel3,
-  rotate: [0, 180],
-  easing: 'linear',
-  duration: 8000,
-  loop: true,
-  translateY: 150
-});
-let wheel3Animation = anime ({
-  targets: wheel4,
-  rotate: [0, -360],
-  easing: 'linear',
-  duration: 20000,
-  loop: true,
-  translateX: 230
-});
+// let wheelAnimation = anime ({
+//   targets: wheel1,
+//   rotate: [0, 360],
+//   easing: 'linear',
+//   duration: 50000,
+//   loop: true
+// });
+// let wheel1Animation = anime ({
+//   targets: wheel2,
+//   rotate: [0, 360],
+//   easing: 'linear',
+//   duration: 5000,
+//   loop: true
+// });
+// let wheel2Animation = anime ({
+//   targets: wheel3,
+//   rotate: [0, 180],
+//   easing: 'linear',
+//   duration: 8000,
+//   loop: true,
+//   translateY: 150
+// });
+// let wheel3Animation = anime ({
+//   targets: wheel4,
+//   rotate: [0, -360],
+//   easing: 'linear',
+//   duration: 20000,
+//   loop: true,
+//   translateX: 230
+// });
